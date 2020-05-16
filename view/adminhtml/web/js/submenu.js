2da8e9dfc3ab06dd2b09811f29b604030fd03477
thedotwriter <peeter.rannou@gmail.com>
2020-05-16 22:02:18 +0200

    Add "Configuration" link to System menu for nostalgic M1 developers.

--- view/adminhtml/web/js/submenu.js
+++ view/adminhtml/web/js/submenu.js
@@ -4,17 +4,18 @@ require([
 ], function ($, modal) {
     $(function () {
         // Prevent the click on the "Configuration" link to load the actual page.
-        $('[data-ui-id=menu-magento-config-system-config] > a').click(function (e) {
-            e.preventDefault();
-        });
+        $('[data-ui-id=menu-magento-config-system-config] > a, [data-ui-id=menu-thedotwriter-storeconfigsubmenu-system-settings-config] > a')
+            .click(function (e) {
+                e.preventDefault();
+            });
 
         // Init the modal that will be opened when clicking on the "Configuration" link.
         $('.config-submenu').modal({
             'type': 'slide',
             'title': 'Configuration',
-            'trigger': '[data-ui-id=menu-magento-config-system-config] > a',
+            'trigger': '[data-ui-id=menu-magento-config-system-config] > a, [data-ui-id=menu-thedotwriter-storeconfigsubmenu-system-settings-config] > a',
             'responsive': true,
             'buttons': []
         });
     });
-});     
+});
